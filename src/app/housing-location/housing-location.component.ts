7ca97748a8833e2354237e8ce35ac48df1d51bf2
Prince Andrew Anire <andrewcustomerhelper@gmail.com>
2024-11-20 18:33:44 +0000

    Update HousingLocationComponent template structure

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from '../housing-location';
 
 @Component({
-    selector: 'app-housing-location',
-    imports: [],
-    template: `
-    <p>
-      housing-location works!
-    </p>
-  `,
-    styles: ``
+  selector: 'app-housing-location',
+  imports: [],
+  template: `<section> </section> `,
+  styles: ``,
 })
 export class HousingLocationComponent {
 
