1becda6d505ef76ed29510540505d9430c8eb22d
Prince Andrew Anire <andrewcustomerhelper@gmail.com>
2024-11-20 04:41:48 +0000

    Add home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  template: `
+  <section>
+    <h1>Find a City</h1>
+  <form>
+    <input type="text" placeholder="Filter by City"/>
+    <button>Search</button>
+  </form>
+  </section>
+  `,
+  styles: ``
+})
+export class HomeComponent {
+
+}
